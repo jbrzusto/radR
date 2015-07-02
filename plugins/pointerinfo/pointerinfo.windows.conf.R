5566352aec37399f002344dfeebe8ff6bc065e2d
john <jbrzusto@fastmail.fm>
2015-07-02 16:29:23 +0000

    fix degree symbol in windows conf file; this was stupidly breaking build on linux

--- plugins/pointerinfo/pointerinfo.windows.conf.R
+++ plugins/pointerinfo/pointerinfo.windows.conf.R
@@ -3,4 +3,5 @@
 ## symbol needs to be different on windows than
 ## in linux
 
-range.bearing.format.string = "range: true=%7.3f km, ground=%7.3f km\nbearing=%5.1f°  elev. angle=%4.1f°"
+range.bearing.format.string = "range: true=%7.3f km, ground=%7.3f km\nbearing=%5.1fÂ°  elev. angle=%4.1fÂ°"
+
