d411327bd53f7da2bc828b9157c6e8430f1ec986
John Brzustowski <john.brzustowski@henryschein.com>
2022-10-01 00:43:30 -0300

    2022: comment-out debugging cruft

--- plugins/inradarch/inradarch.c
+++ plugins/inradarch/inradarch.c
@@ -74,15 +74,14 @@ decompress_sweep (SEXP rawvec, SEXP extptr) {
   numSamples = hdr->samples_per_line * hdr->num_output_lines;
   hdr->compressed_size = LENGTH(rawvec) - 128;
   numDecoded = LZ4_decompress_safe(p + 128, (char *) EXTPTR_PTR(extptr), hdr->compressed_size, hdr->data_size);
-
-  //#ifdef RADR_DEBUG
+#ifdef RADR_DEBUG
   printf("input: %p, output: %p, size:%d, decompsize: %lu, samples_per_line:%d, num_output_lines: %d, numDecoded: %d\n",
          p+128,
          (char *) EXTPTR_PTR(extptr),
          hdr->compressed_size,
          hdr->data_size,
          hdr->samples_per_line, hdr->num_output_lines, numDecoded);
-  //#endif
+#endif
   if (numDecoded != numSamples * sizeof(sample_t))  {
     if (numDecoded == numSamples) {
       // samples in source are 1-byte, so expand them in-place
